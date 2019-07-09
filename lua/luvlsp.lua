ffb3ca22441afbe1f79a0d4bb40b8480c282f5b7
Björn Linse <bjorn.linse@gmail.com>
2019-07-09 22:52:18 +0200

    didChange

--- lua/luvlsp.lua
+++ lua/luvlsp.lua
@@ -94,6 +94,7 @@ function do_init()
       publishDiagnostics={relatedInformation=true},
       -- TODO: signatureInformation
     },
+    offsetEncoding = {'utf-8'}, -- what is WCHAR?
   }
   local p = {
     processId = uv.getpid(),
@@ -108,21 +109,33 @@ function do_init()
   end)
 end
 
+function do_change(bufnr, tick, start, stop, stopped)
+  local uri = "file://"..a.nvim_buf_get_name(bufnr)
+  local version = tick
+  local textDocument = {uri=uri,version=version}
+  local text = table.concat(a.nvim_buf_get_lines(bufnr, start, stopped, true), "\n") .. "\n"
+  local range = {start={line=start,character=0},["end"]={line=stop+1,character=0}}
+  -- what is rangeLength ???
+  local edit = {range=range, text=text}
+  do_notify("textDocument/didChange", {textDocument=textDocument, contentChanges={edit}})
+end
+
 function do_open(bufnr)
   local uri = "file://"..a.nvim_buf_get_name(bufnr)
   local text = table.concat(a.nvim_buf_get_lines(bufnr, 0, -1, true), "\n")
   if a.nvim_buf_get_option(bufnr, 'eol') then text = text..'\n' end
   local version = a.nvim_buf_get_changedtick(bufnr)
   local languageId = "c"
-  local params = {  textDocument = {uri=uri,text=text,version=version,languageId=languageId}}
+  local params = {textDocument = {uri=uri,text=text,version=version,languageId=languageId}}
   safe_print(params)
   do_notify("textDocument/didOpen", params)
+  a.nvim_buf_attach(bufnr, false, {on_lines=function(...) do_change(...) end})
 end
 
 if false then
 uv.write(stdin, "\n")
 do_init()
-do_open(4)
+do_open(2)
 
 do_req("blååååøg", {3}, function (r) safe_print(r.error.message) end)
 --print(vim.inspect(uv))
