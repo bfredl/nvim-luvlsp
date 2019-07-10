1292c24ab5c151078bd3657c04ec1747ea763be2
Björn Linse <bjorn.linse@gmail.com>
2019-07-10 09:20:10 +0200

    state

--- lua/luvlsp.lua
+++ lua/luvlsp.lua
@@ -1,94 +1,92 @@
 do local s, l = pcall(require,'luadev') if s then luadev = l end end
 
-if _G.theid == nil then
-  _G.theid = 0
+if _G.luvlsp == nil then
+  _G.luvlsp = {
+    theid = 0,
+    buffered = '',
+    pending = {},
+  }
 end
+local luvlsp = _G.luvlsp
 
+_G.a = vim.api
+_G.uv = vim.loop
 local a = vim.api
-a = vim.api
+local uv = vim.loop
 
-uv = vim.loop
+luvlsp.d = vim.schedule_wrap((luadev and luadev.print) or print)
 
-safe_print = vim.schedule_wrap((luadev and luadev.print) or print)
+function luvlsp.spawn()
+  local stdin, stdout, stderr = uv.new_pipe(false), uv.new_pipe(false), uv.new_pipe(false)
 
-exepath = "clangd"
-args = {}
+  local function exit_cb()
+    luvlsp.d("very exit")
+  end
 
-stdin = uv.new_pipe(false)
-stdout = uv.new_pipe(false)
-stderr = uv.new_pipe(false)
+  local exepath = "clangd"
+  local args = {}
 
-function lsp_exit_cb()
-  safe_print("very exit")
+  local opts = {args=args, stdio={stdin,stdout,stderr}}
+  luvlsp.handle, luvlsp.pid = uv.spawn(exepath, opts, exit_cb)
+
+  uv.read_start(stdout, function (err, chunk)
+    --luvlsp.d("stdout",chunk, err)
+    if not err then
+      luvlsp.on_stdout(chunk)
+    end
+  end)
+
+  uv.read_start(stderr, function (err, chunk)
+    luvlsp.d("stderr",chunk, err)
+  end)
+
+  luvlsp.stdin = stdin
 end
 
-handle, pid = uv.spawn(exepath,
-                       {args=args,
-                        stdio={stdin,stdout,stderr}},
-                       exit_cb)
-
-uv.read_start(stdout, function (err, chunk)
-  --safe_print("stdout",chunk, err)
-  if not err then
-    on_stdout(chunk)
-  end
-end)
-
-uv.read_start(stderr, function (err, chunk)
-  safe_print("stderr",chunk, err)
-end)
-
-function wmsg(method,params,id)
+function luvlsp.msg(method,params,id)
   local msg = {jsonrpc = "2.0", method = method, params = params, id = id}
   local bytes = a.nvim_call_function('json_encode', {msg})
-  return 'Content-Length: ' .. bytes:len() ..'\r\n\r\n' ..bytes
+  local packet = 'Content-Length: ' .. bytes:len() ..'\r\n\r\n' ..bytes
+  uv.write(luvlsp.stdin, packet)
 end
 
-buffered = ''
-function on_stdout(chunk)
-  state, err = pcall(function()
-  buffered = buffered..chunk
-  local eol = string.find(buffered, '\r\n')
+function luvlsp.on_stdout(chunk)
+  luvlsp.buffered = luvlsp.buffered..chunk
+  local eol = string.find(luvlsp.buffered, '\r\n')
   if not eol then return end
-  line = string.sub(buffered,1,eol-1)
-  space = string.find(line, " ")
-  length = tonumber(string.sub(line,space+1))
+  local line = string.sub(luvlsp.buffered,1,eol-1)
+  local space = string.find(line, " ")
+  local length = tonumber(string.sub(line,space+1))
   -- TODO: can has Content-Type??
-  if string.len(buffered) >= eol + 3 + length then
-    local msg = buffered:sub(eol+2,eol+3+length)
-    buffered = buffered:sub(eol+3+length+1)
-    vim.schedule(function() on_msg(msg) end)
+  if string.len(luvlsp.buffered) >= eol + 3 + length then
+    local msg = luvlsp.buffered:sub(eol+2,eol+3+length)
+    luvlsp.buffered = luvlsp.buffered:sub(eol+3+length+1)
+    vim.schedule(function() luvlsp.on_msg(msg) end)
     -- check again, very tailcall
-    return on_stdout('')
+    return luvlsp.on_stdout('')
   end
-  end)
-  if not state then safe_print(err) end
 end
 
-pending = {}
-function on_msg(bytes)
-  msg = a.nvim_call_function('json_decode', {bytes})
-  safe_print(vim.inspect(msg))
+function luvlsp.on_msg(bytes)
+  local msg = a.nvim_call_function('json_decode', {bytes})
+  luvlsp.d(vim.inspect(msg))
   if msg.id ~= nil then
-    mycb = pending[msg.id]
-    pending[msg.id] = nil
+    local mycb = luvlsp.pending[msg.id]
+    luvlsp.pending[msg.id] = nil
     return mycb(msg)
   end
 end
 
-function do_req(method, params,cb)
-  my_id = _G.theid
-  _G.theid = _G.theid + 1
-  uv.write(stdin, wmsg(method, params, my_id))
-  --safe_print("REQ " ..my_id.. " "..method)
-  pending[my_id] = cb
+function luvlsp.req(method, params, cb)
+  local my_id = luvlsp.theid
+  luvlsp.theid = luvlsp.theid + 1
+  luvlsp.pending[my_id] = cb
+  luvlsp.msg(method, params, my_id)
+  --luvlsp.d("REQ " ..my_id.. " "..method)
 end
 
-function do_notify(method, params)
-  uv.write(stdin, wmsg(method, params))
-end
-
-function do_init()
+function luvlsp.init()
+  luvlsp.spawn()
   local capabilities = {
     textDocument = {
       publishDiagnostics={relatedInformation=true},
@@ -101,7 +99,7 @@ function do_init()
     rootUri = 'file://' .. uv.cwd(),
     capabilities = capabilities,
   }
-  do_req("initialize", p, function(reply)
+  luvlsp.req("initialize", p, function(reply)
     if reply.error then
       error(reply.error.message)
     end
@@ -109,35 +107,34 @@ function do_init()
   end)
 end
 
-function do_change(bufnr, tick, start, stop, stopped)
+function luvlsp.do_change(bufnr, tick, start, stop, stopped)
   local uri = "file://"..a.nvim_buf_get_name(bufnr)
   local version = tick
   local textDocument = {uri=uri,version=version}
   local text = table.concat(a.nvim_buf_get_lines(bufnr, start, stopped, true), "\n") .. "\n"
-  local range = {start={line=start,character=0},["end"]={line=stop+1,character=0}}
+  local range = {start={line=start,character=0},["end"]={line=stop,character=0}}
   -- what is rangeLength ???
   local edit = {range=range, text=text}
-  do_notify("textDocument/didChange", {textDocument=textDocument, contentChanges={edit}})
+  luvlsp.msg("textDocument/didChange", {textDocument=textDocument, contentChanges={edit}})
 end
 
-function do_open(bufnr)
+function luvlsp.do_open(bufnr)
   local uri = "file://"..a.nvim_buf_get_name(bufnr)
   local text = table.concat(a.nvim_buf_get_lines(bufnr, 0, -1, true), "\n")
   if a.nvim_buf_get_option(bufnr, 'eol') then text = text..'\n' end
   local version = a.nvim_buf_get_changedtick(bufnr)
   local languageId = "c"
   local params = {textDocument = {uri=uri,text=text,version=version,languageId=languageId}}
-  safe_print(params)
-  do_notify("textDocument/didOpen", params)
-  a.nvim_buf_attach(bufnr, false, {on_lines=function(...) do_change(...) end})
+  luvlsp.d(params)
+  luvlsp.msg("textDocument/didOpen", params)
+  a.nvim_buf_attach(bufnr, false, {on_lines=function(...) luvlsp.do_change(...) end})
 end
 
 if false then
-uv.write(stdin, "\n")
-do_init()
-do_open(2)
+luvlsp.init()
+luvlsp.do_open(2)
 
-do_req("blååååøg", {3}, function (r) safe_print(r.error.message) end)
+luvlsp.req("blååååøg", {3}, function(r) luvlsp.d(r.error.message) end)
 --print(vim.inspect(uv))
 --uv.flush
 end
