8ab4a8a1557e56a7b1b4ba1cc1df693aba2e356c
Björn Linse <bjorn.linse@gmail.com>
2019-07-10 11:15:41 +0200

    diag

--- lua/luvlsp.lua
+++ lua/luvlsp.lua
@@ -1,19 +1,21 @@
 do local s, l = pcall(require,'luadev') if s then luadev = l end end
 
+_G.a = vim.api
+_G.uv = vim.loop
+local a = vim.api
+local uv = vim.loop
+
 if _G.luvlsp == nil then
   _G.luvlsp = {
     theid = 0,
     buffered = '',
     pending = {},
     shadow = {},
+    bufmap = {},
+    ns = a.nvim_create_namespace("luvlsp"),
   }
 end
 local luvlsp = _G.luvlsp
-
-_G.a = vim.api
-_G.uv = vim.loop
-local a = vim.api
-local uv = vim.loop
 
 luvlsp.d = vim.schedule_wrap((luadev and luadev.print) or print)
 
@@ -68,12 +70,11 @@ function luvlsp.on_stdout(chunk)
   end
 end
 
-function splice(tab,start,stop,new,newend)
+function luvlsp.splice(tab,start,stop,new,newend)
   -- TODO: inefficient
   for i = start,math.min(stop,newend) do
     tab[i] = new[i-start+1]
   end
-  print(vim.inspect(tab))
   if stop > newend then
     for _ = 1, (stop-newend) do
       table.remove(tab,newend+1)
@@ -87,7 +88,7 @@ function splice(tab,start,stop,new,newend)
 end
 
 if false then
-splice({1,2,3,4,5},2,1,{"x","y"},3)
+luvlsp.splice({1,2,3,4,5},2,1,{"x","y"},3)
 end
 
 
@@ -98,8 +99,11 @@ function luvlsp.on_msg(bytes)
     local mycb = luvlsp.pending[msg.id]
     luvlsp.pending[msg.id] = nil
     return mycb(msg)
+  elseif msg.method == "textDocument/publishDiagnostics" then
+    luvlsp.on_diag(msg.params)
   end
 end
+
 
 function luvlsp.req(method, params, cb)
   local my_id = luvlsp.theid
@@ -144,12 +148,13 @@ function luvlsp.do_change(_, bufnr, tick, start, stop, stopped)
     rangeLength = rangeLength + string.len(shadow[i]) + 1
   end
   local edit = {range=range, text=text}--, rangeLength=rangeLength}
-  splice(shadow, start+1,stop,lines,stopped)
+  luvlsp.splice(shadow, start+1,stop,lines,stopped)
   luvlsp.msg("textDocument/didChange", {textDocument=textDocument, contentChanges={edit}})
 end
 
 function luvlsp.do_open(bufnr)
   local uri = "file://"..a.nvim_buf_get_name(bufnr)
+  luvlsp.bufmap[uri] = bufnr
   luvlsp.shadow[bufnr] = a.nvim_buf_get_lines(bufnr, 0, -1, true)
   local text = table.concat(luvlsp.shadow[bufnr], "\n")
   if a.nvim_buf_get_option(bufnr, 'eol') then text = text..'\n' end
@@ -161,6 +166,41 @@ function luvlsp.do_open(bufnr)
   a.nvim_buf_attach(bufnr, false, {on_lines=function(...) luvlsp.do_change(...) end})
 end
 
+
+function luvlsp.on_diag(params)
+  local MessageType = { Error = 1, Warning = 2, Info = 3, Log = 4 }
+  local bufnr = luvlsp.bufmap[params.uri]
+  a.nvim_buf_clear_highlight(bufnr, luvlsp.ns, 0, -1)
+  local last_line, last_severity = -1, -1
+  for _, msg in ipairs(params.diagnostics) do
+    local range = msg.range
+    range._end = range['end']
+    if range._end.line ~= range.start.line then
+      range._end.line = range.start.line
+      range._end.character = -1
+    end
+
+    if last_line == range.start.line and msg.severity > last_severity then
+      goto continue
+    end
+    last_line, last_severity = range.start.line, msg.severity
+
+    local msg_hl
+    if msg.severity == MessageType.Error then
+      msg_hl = "LspError"
+    elseif msg.severity == MessageType.Warning then
+      msg_hl = "LspWarning"
+    else
+      msg_hl = "LspOtherMsg"
+    end
+    a.nvim_buf_set_virtual_text(bufnr, luvlsp.ns, range.start.line, {{'▶ '..msg.message, msg_hl}}, {})
+
+    local loc_hl = "LspLocation"
+    a.nvim_buf_add_highlight(bufnr, luvlsp.ns, loc_hl, range.start.line, range.start.character, range._end.character)
+    ::continue::
+  end
+
+end
 if false then
 luvlsp.init()
 luvlsp.do_open(2)
---
+++ plugin/luvlsp.vim
@@ -0,0 +1,6 @@
+hi default LspWarning guifg=#aaaa00
+hi default LspError guifg=#ee2222
+hi default LspOtherMsg guifg=#888888
+hi default LspLocation gui=underline
+hi default LspLocationError gui=underline guibg=#660000
+hi default LspLocationWarning gui=underline guibg=#222200
