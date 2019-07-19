b9a28741646615baa5ab3cbddeb8720267948776
Björn Linse <bjorn.linse@gmail.com>
2019-07-19 16:01:08 +0200

    WIP: sig

--- lua/luvlsp/feat.lua
+++ lua/luvlsp/feat.lua
@@ -1,3 +1,4 @@
+local a = vim.api
 if _G.luvlsp_feat == nil then
   _G.luvlsp_feat = {
     diag_ns = a.nvim_create_namespace("luvlsp/diag"),
@@ -9,16 +10,23 @@ local luvlsp = require'luvlsp'
 
 luvlsp_feat.textDocument_caps = {
   publishDiagnostics={relatedInformation=true},
-  -- TODO: signatureInformation
+  signatureHelp = {
+    signatureInformation = {
+      documentationFormat = { 'plaintext' },
+      parameterInformation = {
+        labelOffsetSupport = true,
+      },
+    },
+  },
 }
 
-function luvlsp_feat.on_diag(msg)
+function luvlsp_feat.on_diag(data)
   local ns = luvlsp_feat.diag_ns
   local MessageType = { Error = 1, Warning = 2, Info = 3, Log = 4 }
-  local bufnr = luvlsp.bufmap[msg.params.uri]
+  local bufnr = luvlsp.bufmap[data.params.uri]
   a.nvim_buf_clear_highlight(bufnr, ns, 0, -1)
   local last_line, last_severity = -1, -1
-  for _, msg in ipairs(msg.params.diagnostics) do
+  for _, msg in ipairs(data.params.diagnostics) do
     local range = msg.range
     range._end = range['end']
     if range._end.line ~= range.start.line then
@@ -48,7 +56,33 @@ function luvlsp_feat.on_diag(msg)
 end
 luvlsp.msg_handlers["textDocument/publishDiagnostics"] = luvlsp_feat.on_diag
 
+function luvlsp_feat.pos_params(winid)
+  if winid == nil then winid = 0 end
+  local bufnr = a.nvim_win_get_buf(winid)
+  local uri = "file://"..a.nvim_buf_get_name(bufnr)
+  local cursor = a.nvim_win_get_cursor(winid)
+  return {
+    textDocument = {uri=uri},
+    position = {line=cursor[1]-1, character=cursor[2]}
+  }
+end
+
 function luvlsp_feat.do_signature()
+  local position = luvlsp_feat.pos_params()
+  luvlsp.req("textDocument/signatureHelp",position,function(msg)
+    info = msg.result
+    if info.activeSignature == nil or #info.signatures <= info.activeSignature then
+      return
+    end
+    signature = info.signatures[info.activeSignature+1]
+
+    if info.activeParameter and #signature.parameters > info.activeParameter then
+      parameter = signature.parameters[info.activeParameter+1]
+      if type(parameter.label) == "string" then
+        startpos = nil
+      end
+    end
+  end)
 end
 
 return luvlsp_feat
